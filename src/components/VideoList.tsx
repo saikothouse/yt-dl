118c40862c627bcdfd783c60a1bb9c20bcacb285
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-04 02:29:31 +0600

    Update VideoList.tsx

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaPlay } from 'react-icons/fa';
 
 const VideoListContainer = styled.div`
   display: flex;
@@ -11,6 +12,28 @@ const VideoListContainer = styled.div`
 const VideoItem = styled.div`
   width: 300px;
   margin: 10px;
+  background-color: white;
+  padding: 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+  text-align: center;
+`;
+
+const Thumbnail = styled.img`
+  width: 100%;
+  border-radius: 10px;
+`;
+
+const VideoTitle = styled.h3`
+  font-size: 1.2em;
+  margin: 10px 0;
+  color: #282c34;
+`;
+
+const VideoLink = styled.a`
+  color: #61dafb;
+  display: inline-block;
+  margin-top: 10px;
 `;
 
 interface Video {
@@ -36,9 +59,11 @@ const VideoList: React.FC<VideoListProps> = ({ videos }) => {
     <VideoListContainer>
       {videos.map(video => (
         <VideoItem key={video.id.videoId}>
-          <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
-          <h3>{video.snippet.title}</h3>
-          <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer">Watch</a>
+          <Thumbnail src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+          <VideoTitle>{video.snippet.title}</VideoTitle>
+          <VideoLink href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer">
+            <FaPlay /> Watch
+          </VideoLink>
         </VideoItem>
       ))}
     </VideoListContainer>
