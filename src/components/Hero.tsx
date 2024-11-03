601f64d48943c9225fb054c8609c3fc8b103ed9b
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-04 03:12:34 +0600

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,18 +11,15 @@ const HeroContainer = styled.div`
   color: white;
   text-align: center;
   padding: 20px;
-  box-sizing: border-box;
-
-  @media (max-width: 600px) {
-    padding: 10px;
+  @media (max-width: 768px) {
     height: 50vh;
+    padding: 10px;
   }
 `;
 
 const HeroTitle = styled.h1`
   font-size: 3em;
-
-  @media (max-width: 600px) {
+  @media (max-width: 768px) {
     font-size: 2em;
   }
 `;
@@ -30,10 +27,8 @@ const HeroTitle = styled.h1`
 const HeroSubtitle = styled.p`
   font-size: 1.5em;
   margin-top: 10px;
-
-  @media (max-width: 600px) {
-    font-size: 1.2em;
-    margin-top: 5px;
+  @media (max-width: 768px) {
+    font-size: 1em;
   }
 `;
 
